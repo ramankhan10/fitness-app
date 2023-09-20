f513ceb86acfc474671762867697651d00046dbf
Raman <khanahmadi.raman@yahoo.com>
2023-09-20 21:32:59 +0330

    complete auth Guard

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,29 @@
+import {
+  ActivatedRouteSnapshot,
+  Router,
+  RouterStateSnapshot,
+  CanActivateFn,
+} from '@angular/router';
+import { AuthService } from './auth.service';
+import { Injectable, inject } from '@angular/core';
+
+@Injectable()
+export class PermissionService {
+  constructor(private router: Router, private authService: AuthService) {}
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.authService.isAuth()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
+
+export const AuthGuard: CanActivateFn = (
+  next: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  return inject(PermissionService).canActivate(next, state);
+};
