77c5a4d96b8b0cf36b45f4636638f3902692d3ea
Raman <khanahmadi.raman@yahoo.com>
2023-09-22 21:02:52 +0330

    complete persian paginator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule} from '@angular/forms';
-import {MaterialModule} from './material.module';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import {
   DateAdapter,
   MAT_DATE_FORMATS,
@@ -17,20 +17,22 @@ import {
   PERSIAN_DATE_FORMATS,
 } from './persian-dateadapter';
 
-import {LoginComponent} from './auth/login/login.component';
-import {SignupComponent} from './auth/signup/signup.component';
-import {TrainingComponent} from './training/training.component';
-import {NewTrainingComponent} from './new-training/new-training.component';
-import {CurrentTrainingComponent} from './current-training/current-training.component';
-import {PastTrainingComponent} from './past-training/past-training.component';
-import {WelcomeComponent} from './welcome/welcome.component';
-import {HeaderComponent} from './navigation/header/header.component';
-import {SidenavListComponent} from './navigation/sidenav-list/sidenav-list.component';
-import {StopTrainingComponent} from './current-training/stop-training/stop-training.component';
-import {AuthService} from "./auth/auth.service";
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { TrainingComponent } from './training/training.component';
+import { NewTrainingComponent } from './new-training/new-training.component';
+import { CurrentTrainingComponent } from './current-training/current-training.component';
+import { PastTrainingComponent } from './past-training/past-training.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { StopTrainingComponent } from './current-training/stop-training/stop-training.component';
+import { AuthService } from './auth/auth.service';
 import { TrainingService } from './training/training.service';
 import { PersianDatePipe } from './persian-date.pipe';
 import { TranslationPipe } from './translation.pipe';
+import { MatPaginatorIntl } from '@angular/material/paginator';
+import { PersianPaginatorIntl } from './pagination.translate';
 
 @NgModule({
   declarations: [
@@ -46,7 +48,7 @@ import { TranslationPipe } from './translation.pipe';
     SidenavListComponent,
     StopTrainingComponent,
     PersianDatePipe,
-    TranslationPipe
+    TranslationPipe,
   ],
   imports: [
     BrowserModule,
@@ -58,6 +60,10 @@ import { TranslationPipe } from './translation.pipe';
   ],
   providers: [
     {
+      provide: MatPaginatorIntl,
+      useClass: PersianPaginatorIntl,
+    },
+    {
       provide: DateAdapter,
       useClass: MaterialPersianDateAdapter,
       deps: [MAT_DATE_LOCALE],
@@ -66,8 +72,9 @@ import { TranslationPipe } from './translation.pipe';
       provide: MAT_DATE_FORMATS,
       useValue: PERSIAN_DATE_FORMATS,
     },
-    AuthService, TrainingService],
+    AuthService,
+    TrainingService,
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
---
+++ src/app/pagination.translate.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { MatPaginatorIntl } from '@angular/material/paginator';
+
+@Injectable()
+export class PersianPaginatorIntl extends MatPaginatorIntl {
+  override itemsPerPageLabel = 'مورد در هر صفحه';
+  override nextPageLabel = 'صفحه بعد ';
+  override previousPageLabel = 'صفحه قبل';
+  override firstPageLabel = 'صفحه اول';
+  override lastPageLabel = 'صفحه آخر';
+
+  override getRangeLabel = (page: number, pageSize: number, length: number) => {
+    const start = page * pageSize + 1;
+    const end = Math.min((page + 1) * pageSize, length);
+
+    return `${start} - ${end} از ${length}`;
+  };
+}
