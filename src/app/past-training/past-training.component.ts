8d9aa72c5ed5bcf950ddc9a8e7c5127063bd04a0
Raman <khanahmadi.raman@yahoo.com>
2023-09-22 12:06:16 +0330

    complete sort filter table

--- src/app/past-training/past-training.component.ts
+++ src/app/past-training/past-training.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { TrainingService } from '../training/training.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { Exercise } from '../training/exercise.model';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-past-training',
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.scss'],
 })
-export class PastTrainingComponent {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = [
     'name',
     'duration',
@@ -14,8 +17,22 @@ export class PastTrainingComponent {
     'date',
     'state',
   ];
+  dataSource = new MatTableDataSource<Exercise>();
+
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(private trainingService: TrainingService) {}
 
-  dataSource = this.trainingService.getCompleteOrCancelExercises();
+  ngOnInit(): void {
+    this.dataSource.data = this.trainingService.getCompleteOrCancelExercises();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLocaleLowerCase()
+  }
 }
